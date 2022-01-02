25a054a449a93f7454fb658b26d55d8bdec738ff
Shrudin Suresh <92779132+Shrud-in@users.noreply.github.com>
2022-01-02 21:21:17 +0530

    Update factorial with iteration.py

--- factorial with iteration.py
+++ factorial with iteration.py
@@ -1,8 +1,8 @@
 def factorial(n):
     result = 1
     for x in range(1,n):
-        result = x * result
+        result = x *result
     return result
 
-for n in range(0,11):
+for n in range(0,10):
     print(n, factorial(n+1))
